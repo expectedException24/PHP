d7c5750113dd7f498e8c7d909556b0f31a617ce3
Gabriel <gabrielsiano2006@gmail.com>
2025-10-07 07:41:03 +0200

    es 5 terminato

--- es_5_gerini/index.php
+++ es_5_gerini/index.php
@@ -1,5 +1,6 @@
     <?php
-        $GRADES=[7, 5, 6, 8, 9]; // da finire 
+        $grades=[7, 5, 6, 6, 5]; // da finire 
+        include 'function.php';
     ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,15 +12,13 @@
 </head>
 <body>
     <?php
-        echo "<p style='color: green; font-size: " . rand(16, 40) . "px;'>$str</p>";
-        echo "<p style='color: blue; font-size: " . rand(16, 40) . "px;'>".strtoupper($str)."</p>";
+        $mean=mean($grades);
+        echo "<p>La media dello studente è: ".$mean."</p>";
     ?>
     <?php
-        echo"<h1 style='color: red;'>Il paragrafo ha ".str_word_count($str)." parole </h1>";
-        echo"<h1 style='color: yellow;'>Il paragrafo ha ".strlen($str)." parole </h1>";
+        echo "<p>Lo studente è: ".getResult($mean)."</p>";
     ?>
 
-    
 </body>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
