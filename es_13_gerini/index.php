109a4c0962cf20eeba6b2caf422e1bd88129ce3c
expectedException24 <agerini36@gmail.com>
2025-11-25 09:33:55 +0100

    da fare form

--- es_13_gerini/index.php
+++ es_13_gerini/index.php
@@ -9,15 +9,27 @@
 </head>
 <body>
     <?php
-        foreach ($quiz as $q){
-            echo $q["question"];
-            foreach($q["answers"] as $ans){
-                echo $ans;
+        foreach ($quiz as $index => $q) {
+            echo "<div style='margin-bottom:20px; padding:15px; border:1px solid #ccc; border-radius:8px;'>";
+            echo "<h3 style='margin-top:0;'>" . htmlspecialchars($q["question"]) . "</h3>";
+            echo "<ul style='list-style:none; padding-left:0;'>";
+            foreach ($q["answers"] as $aIndex => $ans) {
+                echo "
+                    <li style='margin:6px 0;'>
+                        <label>
+                            <input type='checkbox' 
+                                name='q{$index}[]' 
+                                value='{$aIndex}' 
+                                style='margin-right:6px;'>
+                            " . htmlspecialchars($ans) . "
+                        </label>
+                    </li>
+                ";
             }
+            echo "</ul>";
+            echo "</div>";
         }
-
-
-
     ?>
+
 </body>
 </html>
